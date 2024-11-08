6d8b2bb3f740223853fa0481dbebabfc22fd56c0
Anukiw2002 <anuki.thiyara@gmail.com>
2024-11-08 13:45:57 +0530

    Sign up page 3

--- src/main/java/org/example/demo2/servlet/TemporaryServlet.java
+++ src/main/java/org/example/demo2/servlet/TemporaryServlet.java
@@ -19,8 +19,8 @@ public class TemporaryServlet extends HttpServlet {
             request.getRequestDispatcher("/WEB-INF/views/client/Dummy.jsp").forward(request, response);
         } else if ("page2".equals(page)) {
             request.getRequestDispatcher("/WEB-INF/views/client/signUp2.jsp").forward(request, response);
-        } else if ("try".equals(page)) {
-            request.getRequestDispatcher("/WEB-INF/views/client").forward(request, response);
+        } else if ("page3".equals(page)) {
+            request.getRequestDispatcher("/WEB-INF/views/client/signUp3.jsp").forward(request, response);
         } else if ("ownerDashboard".equals(page)) {
             request.getRequestDispatcher("/WEB-INF/views/owner - dashboard.jsp").forward(request, response);
         } else {
