2e64c8a742999f679a81d85dcaef7e9d43aa1588
Anukiw2002 <anuki.thiyara@gmail.com>
2024-11-19 08:50:38 +0530

    Seperating the Membership servlet into 4 different codes

---
+++ src/main/java/org/example/demo2/servlet/ViewMembershipPlanServlet.java
@@ -0,0 +1,19 @@
+package org.example.demo2.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import java.io.IOException;
+
+
+@WebServlet("/membership/view")
+public class ViewMembershipPlanServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/owner/viewMembershipPlans.jsp")
+                .forward(request, response);
+    } }
