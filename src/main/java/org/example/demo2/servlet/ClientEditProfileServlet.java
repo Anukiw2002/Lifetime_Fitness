c0d6db428fb6f0a1fe5c7aaf9a788e99c5e36345
Ishan maduranga <ishanmadurangai662@gmail.com>
2024-11-25 14:05:53 +0530

    some modifications maded in the side navbar

--- src/main/java/org/example/demo2/servlet/ClientEditProfileServlet.java
+++ src/main/java/org/example/demo2/servlet/ClientEditProfileServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 @WebServlet("/editProfile")
-public class EditProfileServlet extends HttpServlet {
+public class ClientEditProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/views/client/editProfile.jsp").forward(request, response);
     }
