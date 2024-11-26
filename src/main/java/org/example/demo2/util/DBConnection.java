19518803c28ed0d2d8d2743b752428367b57cdba
gamiduwelikanna <gamidukaveen@gmail.com>
2024-11-26 18:39:30 +0530

    26 NOV-8

--- src/main/java/org/example/demo2/util/DBConnection.java
+++ src/main/java/org/example/demo2/util/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/auth_db";
+    private static final String URL = "jdbc:postgresql://localhost:6000/LifeTime_Fitness";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Ishn@2002";
+    private static final String PASSWORD = "45121556Aa#";
 
     static {
         try {
