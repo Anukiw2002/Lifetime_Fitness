1581399f40ab7f939c9a93c9517be9ac7316c754
Anukiw2002 <anuki.thiyara@gmail.com>
2024-11-26 16:37:23 +0530

    Made the styles consistent

--- src/main/java/org/example/demo2/util/DBConnection.java
+++ src/main/java/org/example/demo2/util/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:6000/LifeTime_Fitness";
+    private static final String URL = "jdbc:postgresql://localhost:5432/LifetimeFitness";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "45121556Aa#";
+    private static final String PASSWORD = "Admin";
 
     static {
         try {
