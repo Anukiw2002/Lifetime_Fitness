086a70926622e17933934217a9125d04bade8d0c
Ishan maduranga <ishanmadurangai662@gmail.com>
2024-11-27 14:37:54 +0530

    maded modifications

--- src/main/java/org/example/demo2/servlet/OwnerDetailedNotificationsServlet.java
+++ src/main/java/org/example/demo2/servlet/OwnerDetailedNotificationsServlet.java
@@ -12,6 +12,6 @@ import java.io.IOException;
 public class OwnerDetailedNotificationsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/owner/ownerDetailedNotifications").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/owner/ownerDetailedNotifications.jsp").forward(req, resp);
     }
 }
