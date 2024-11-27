e709b8974d75226b403954f7006317b92c0f76a1
KavinduPathiratne09 <kavindupathiratne@gmail.com>
2024-11-27 19:18:23 +0530

    create instructor member management

--- src/main/java/org/example/demo2/util/DBConnection.java
+++ src/main/java/org/example/demo2/util/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/auth_db";
+    private static final String URL = "jdbc:postgresql://localhost:5432/LifetimeFitness";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Ishn@2002";
+    private static final String PASSWORD = "user123";
 
     static {
         try {
