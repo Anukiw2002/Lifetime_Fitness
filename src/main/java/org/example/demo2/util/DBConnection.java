de064976db0fbfdfae097eb1057308bed7cf395c
Ishan maduranga <ishanmadurangai662@gmail.com>
2025-02-13 18:31:06 +0530

    changed the name of the sym

--- src/main/java/org/example/demo2/util/DBConnection.java
+++ src/main/java/org/example/demo2/util/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/auth_db";
+    private static final String URL = "jdbc:postgresql://localhost:5432/gym";
     private static final String USER = "postgres";
     private static final String PASSWORD = "Ishn@2002";
 
