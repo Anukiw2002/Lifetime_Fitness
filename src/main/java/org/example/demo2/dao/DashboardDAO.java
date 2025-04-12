e26f3ed8ff1ad8fc727572c3cb8eeb0a19cd3db0
Anuki Wanniarachchi <anuki.thiyara@gmail.com>
2025-04-12 11:06:12 +0530

    Created the retreivals needed for the membership profile page

--- src/main/java/org/example/demo2/dao/DashboardDAO.java
+++ src/main/java/org/example/demo2/dao/DashboardDAO.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DashboardDAO {
@@ -29,7 +30,7 @@ public class DashboardDAO {
     }
 
     public int getWorkoutSteakById(Integer user_id){
-        List<LocalDate> workoutDates = new ArrrayList<>();
+        List<LocalDate> workoutDates = new ArrayList<>();
 
         String query = "SELECT DISTINCT created_at FROM client_workouts WHERE user_id = ? ORDER BY created_at DESC";
 
