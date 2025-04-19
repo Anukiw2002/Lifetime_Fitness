79823cbc36ddb33b6330ed15ae425cb5b34fca6b
Ishan maduranga <ishanmadurangai662@gmail.com>
2025-04-19 07:56:30 +0530

    created the function in dao to get report count

--- src/main/java/org/example/demo2/dao/InstructorDashboardDAO.java
+++ src/main/java/org/example/demo2/dao/InstructorDashboardDAO.java
@@ -1,5 +1,6 @@
 package org.example.demo2.dao;
 
+import jakarta.servlet.jsp.jstl.sql.SQLExecutionTag;
 import org.example.demo2.util.DBConnection;
 
 import java.sql.Connection;
@@ -26,6 +27,21 @@ public class InstructorDashboardDAO {
     public int getWorkouts() throws SQLException{
         int count = -1;
         String sql = "SELECT COUNT(*) AS count FROM client_workouts WHERE created_at = CURRENT_DATE";
+        try(Connection conn = DBConnection.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql)){
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()){
+                count = rs.getInt("count");
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+    public int getReports() throws SQLException{
+        int count = -1;
+        String sql = "SELECT COUNT(*) AS count FROM user_reports";
         try(Connection conn = DBConnection.getConnection();
         PreparedStatement stmt = conn.prepareStatement(sql)){
             ResultSet rs = stmt.executeQuery();
