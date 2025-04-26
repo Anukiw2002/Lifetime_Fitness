518e8d45338b41b25e7a232fb5abd027cab18a04
Anuki Wanniarachchi <anuki.thiyara@gmail.com>
2025-04-26 21:03:38 +0530

    Changes done while testing member profile

--- src/main/java/org/example/demo2/util/DBConnection.java
+++ src/main/java/org/example/demo2/util/DBConnection.java
@@ -5,11 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/DB";
 
-    // Update with Railway PostgreSQL connection details
-    private static final String URL = "jdbc:postgresql://localhost:6000/NewDB25";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "45121556Aa#";// Use your actual password
+    private static final String PASSWORD = "Admin";
 
     static {
         try {
