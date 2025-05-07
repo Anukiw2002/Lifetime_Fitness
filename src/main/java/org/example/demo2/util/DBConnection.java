78a9ff414cf58360d26f4b6078ae66649c5ef7c9
KavinduPathiratne09 <kavindupathiratne@gmail.com>
2025-05-07 20:23:48 +0530

    blah blah blah

--- src/main/java/org/example/demo2/util/DBConnection.java
+++ src/main/java/org/example/demo2/util/DBConnection.java
@@ -5,18 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    // Update with Railway PostgreSQL connection details
 
-    private static final String URL = "jdbc:postgresql://localhost:6000/NewDB25";
+    private static final String URL = "jdbc:postgresql://localhost:5432/LifetimeFitnessLocal";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "45121556Aa#";
+    private static final String PASSWORD = "user123";
 
-
-    /*
-    private static final String URL = "jdbc:postgresql://shuttle.proxy.rlwy.net:12024/railway";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "zetNnHzGDoSIcaAzhVINNoCzsrJlyNGk"; // Use your actual password
-    */
 
     static {
         try {
